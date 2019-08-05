613596edf169aa3a4689f7dc865e2da6161cb30a
linkk-mac <linkeke_sea@163.com>
2019-08-05 08:45:01 +0800

    demo

--- demo 2/src/main/java/com/example/demo/util/http/HttpClientUtil.java
+++ demo 2/src/main/java/com/example/demo/util/http/HttpClientUtil.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Map;
 
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
@@ -11,6 +12,7 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
@@ -18,21 +20,54 @@ import org.slf4j.LoggerFactory;
 
 public class HttpClientUtil {
 	private static Logger logger = LoggerFactory.getLogger(HttpClientUtil.class);
-	
+
 	public static String doGet(String httpUrl) {
-		String result="";
-		RequestConfig requestConfig = RequestConfig.custom().
-			    setConnectionRequestTimeout(1000).setConnectTimeout(1000).setSocketTimeout(1000).build();
-		HttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
-		HttpGet httpGet = new HttpGet(httpUrl); 
+		String result = "";
+		HttpClient client = null;
+		HttpResponse httpResponse = null;
+		RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(1000).setConnectTimeout(1000)
+				.setSocketTimeout(1000).build();
+		client = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
+		HttpGet httpGet = new HttpGet(httpUrl);
 		try {
-			HttpResponse httpResponse = client.execute(httpGet);
+			httpResponse = client.execute(httpGet);
 			result = EntityUtils.toString(httpResponse.getEntity());
 		} catch (ClientProtocolException e) {
-			logger.error("HttpClientUtil doGet exception",e);
+			logger.error("HttpClientUtil doGet exception", e);
 		} catch (IOException e) {
-			logger.error("HttpClientUtil doGet IOException",e);
+			logger.error("HttpClientUtil doGet IOException", e);
+		}finally {
+			httpGet.releaseConnection();
 		}
 		return result;
+	}
+	
+	public static String doPost(String httpUrl,Map<String,Object> param) {
+		String result="";
+		HttpClient client = null;
+		HttpResponse httpResponse = null;
+		RequestConfig requestConfig = getRequestConfig();
+		client = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
+		HttpPost httpPost = new HttpPost(httpUrl);
+		try {
+			httpResponse = client.execute(httpPost);
+			result = EntityUtils.toString(httpResponse.getEntity());
+		} catch (ClientProtocolException e) {
+			logger.error("HttpClientUtil doGet exception", e);
+		} catch (IOException e) {
+			logger.error("HttpClientUtil doGet IOException", e);
+		}finally {
+			httpPost.releaseConnection();
+		}
+		
+		
+		
+		return result;
+	}
+	
+	private static RequestConfig getRequestConfig() {
+		RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(1000).setConnectTimeout(1000)
+				.setSocketTimeout(1000).build();
+		return requestConfig;
 	}
 }
