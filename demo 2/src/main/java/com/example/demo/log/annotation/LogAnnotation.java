ac60aec0da80c8f6acc48a6d3484656496b2bf8d
linkk-mac <linkeke_sea@163.com>
2019-09-08 22:09:56 +0800

    annotation

--- demo 2/src/main/java/com/example/demo/log/annotation/LogAnnotation.java
+++ demo 2/src/main/java/com/example/demo/log/annotation/LogAnnotation.java
@@ -2,6 +2,7 @@ package com.example.demo.log.annotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -9,6 +10,7 @@ import java.lang.annotation.Target;
 @Documented
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface LogAnnotation {
 	String value() default "default";
 }
