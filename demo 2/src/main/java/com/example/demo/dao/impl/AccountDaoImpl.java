6a46121a28e87f304542113cf9aa21b01824b671
linkk-mac <linkeke_sea@163.com>
2019-09-17 08:03:44 +0800

    dao

---
+++ demo 2/src/main/java/com/example/demo/dao/impl/AccountDaoImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.dao.impl;
+
+import com.example.demo.dao.AccountDao;
+
+public class AccountDaoImpl implements AccountDao {
+
+	@Override
+	public void register() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
