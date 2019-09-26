1abd45cf4092178dafc82f2f8e192820b681dbaf
linkk-mac <linkeke_sea@163.com>
2019-09-26 08:49:01 +0800

    encrypt

--- demo 2/src/main/java/com/example/demo/field/interceptor/FiledEncyptInterceptor.java
+++ demo 2/src/main/java/com/example/demo/field/interceptor/FiledEncyptInterceptor.java
@@ -1,9 +1,13 @@
 package com.example.demo.field.interceptor;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.sql.PreparedStatement;
+import java.util.Objects;
 import java.util.Properties;
 
 import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
@@ -11,17 +15,48 @@ import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.stereotype.Component;
 
-@Intercepts({@Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})})
+import com.example.demo.enums.DESType;
+import com.example.demo.field.annotation.EncryptDecryptClass;
+import com.example.demo.util.encrpyt.DESUtil;
+
+@Intercepts({@Signature(type = ParameterHandler.class, method = "update", args = {PreparedStatement.class, Object.class})})
+@ConditionalOnProperty(value = "domain.encrypt", havingValue = "true")
+@Component
 public class FiledEncyptInterceptor implements Interceptor {
 
 	private static Logger logger = LoggerFactory.getLogger(FiledEncyptInterceptor.class);
+	private static String hh = "uuuuuuuu";
 	
 	@Override
 	public Object intercept(Invocation invocation) throws Throwable {
-		MappedStatement mappedStatement = (MappedStatement)invocation.getArgs()[0];
-		String name = invocation.getMethod().getName();
-		return null;
+		
+		if(invocation.getTarget() instanceof ParameterHandler) {
+			ParameterHandler parameterHandler = (ParameterHandler) invocation.getTarget();
+			PreparedStatement ps = (PreparedStatement) invocation.getArgs()[0];
+			
+			// 反射获取 参数对像
+			Field parameterField =
+					parameterHandler.getClass().getDeclaredField("parameterObject");
+			parameterField.setAccessible(true);
+			Object parameterObject = parameterField.get(parameterHandler);
+			if (Objects.nonNull(parameterObject)){
+				Class<?> parameterObjectClass = parameterObject.getClass();
+				EncryptDecryptClass encryptDecryptClass = AnnotationUtils.findAnnotation(parameterObjectClass, EncryptDecryptClass.class);
+				if (Objects.nonNull(encryptDecryptClass)){
+					Field[] declaredFields = parameterObjectClass.getDeclaredFields();
+
+					byte[] enkey3 = DESUtil.initKey(DESType.DES3.getValue(),DESType.DES3.getKeySize());
+					byte[] encryptDES3 = DESUtil.encryptDES(hh.getBytes(),enkey3,DESType.DES3.getValue());
+					
+					final Object encrypt = encryptDecrypt.encrypt(declaredFields, parameterObject);
+				}
+			}
+		}
+		return invocation.proceed();
 	}
 
 	@Override
